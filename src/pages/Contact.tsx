2a7a4b22025371f441e5321876e3bc83ab5e52d8
mohbzour99-max <mohbzour99@gmail.com>
2025-08-13 17:22:41 +0300

    Create missing Contact page

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,17 @@
+import Navigation from '@/components/Navigation';
+import ContactSection from '@/components/ContactSection';
+import Footer from '@/components/Footer';
+
+const Contact = () => {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <div className="pt-16">
+        <ContactSection />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
